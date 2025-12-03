7cae36516d5b3650b942c54c511a27cde669af25
Shashank Raj Chavan <shashankrajchavan@Shashanks-MacBook-Air.local>
2025-12-03 16:21:00 +0530

    Add package.json and update wrangler.toml for Cloudflare Pages deployment

--- worker.js
+++
@@ -1,9 +0,0 @@
-// Minimal worker to serve static site
-// This is required when using 'wrangler deploy' instead of 'wrangler pages deploy'
-
-export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request);
-  },
-};
-
